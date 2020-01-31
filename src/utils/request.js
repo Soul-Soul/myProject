4ade5d0abff0e0e57b3902efaee66b46ac45ade4
Mr.ZhaoXiaoJie <zhaoxiaojie1993@163.com>
2020-01-31 22:47:54 +0800

    初次修改，架构

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
+import { Message } from 'view-design'
+// import { getToken, removeToken, getSecret, removeSecret } from '@/utils/auth'
+
 
 let apiUrl = ''
 let HOST = process.env.HOST
 HOST = HOST === 'prod' ? '' : '-test'
 apiUrl = 'https://api' + HOST + '.weifengqi18.com'
 
-// 创建axios实例
 const service = axios.create({
-    baseURL: apiUrl,    //填写域名
+    baseURL: apiUrl, //填写域名
     // baseURL: 'http://192.168.31.199:8081', //局域网开发
     timeout: 30000, // 请求超时时间
     headers: {
@@ -15,83 +17,45 @@ const service = axios.create({
     }
 })
 
-// request拦截器
+
+//http request 拦截器
 service.interceptors.request.use(
     config => {
-        // 根据条件加入token-安全携带
-        if (true) { // 需自定义
-            // 让每个请求携带token
-            config.headers['User-Token'] = '';
+        config.data = JSON.stringify(config.data);
+        /*if (getSecret() && getSecret() !== undefined) {
+            config.headers['pweb-login-secret-key'] = getSecret() // 让每个请求携带自定义请求头
         }
-        return config;
+        if (getToken() && getToken() !== undefined) {
+            config.headers['PWEB-TICKETID'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+        }*/
+        return config
     },
     error => {
-        // 请求错误处理
-        Promise.reject(error);
-    }
-)
-// respone拦截器
+        // Do something with request error
+        console.log(error) // for debug
+        Promise.reject(error)
+    })
+
+//响应拦截器即异常处理
 service.interceptors.response.use(
     response => {
-        // 统一处理状态
-        const res = response.data;
-        if (res.statuscode != 1) { // 需自定义
-            // 返回异常
-            return Promise.reject({
-                status: res.statuscode,
-                message: res.message
-            });
-        } else {
-            return response.data;
-        }
+       /* /!**
+         * code为03是登录失效 可结合自己业务进行修改
+         *!/
+        if (response.data.result === '03' || response.data.result === '04') {
+            removeToken()
+            removeSecret()
+            vm.$router.push({ path: '/login' })
+        }*/
+        return response
     },
-    // 处理处理
     error => {
-        if (error && error.response) {
-            switch (error.response.status) {
-                case 400:
-                    error.message = '错误请求';
-                    break;
-                case 401:
-                    error.message = '未授权，请重新登录';
-                    break;
-                case 403:
-                    error.message = '拒绝访问';
-                    break;
-                case 404:
-                    error.message = '请求错误,未找到该资源';
-                    break;
-                case 405:
-                    error.message = '请求方法未允许';
-                    break;
-                case 408:
-                    error.message = '请求超时';
-                    break;
-                case 500:
-                    error.message = '服务器端出错';
-                    break;
-                case 501:
-                    error.message = '网络未实现';
-                    break;
-                case 502:
-                    error.message = '网络错误';
-                    break;
-                case 503:
-                    error.message = '服务不可用';
-                    break;
-                case 504:
-                    error.message = '网络超时';
-                    break;
-                case 505:
-                    error.message = 'http版本不支持该请求';
-                    break;
-                default:
-                    error.message = `未知错误${error.response.status}`;
-            }
-        } else {
-            error.message = "连接到服务器失败";
-        }
-        return Promise.reject(error);
-    }
-)
+        console.log('err' + error)
+        Message({
+            message: error.message,
+            type: 'error',
+            duration: 5 * 1000
+        })
+        return Promise.reject(error)
+    })
 export default service
